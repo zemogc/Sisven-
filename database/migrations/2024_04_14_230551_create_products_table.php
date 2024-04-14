0499de94eab362df9cc38703aee73a3bfe7dca8c
Zemog gomez <zemogmjg@gmail.com>
2024-04-14 18:16:31 -0500

    Migraciones

---
+++ database/migrations/2024_04_14_230551_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 64)->unique();
+            $table->integer('price')->nullable(false); 
+            $table->integer('stock')->nullable(false); 
+            $table->unsignedBigInteger('category_id')->nullable(false); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
